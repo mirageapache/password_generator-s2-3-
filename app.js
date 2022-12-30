b8776f8dd3d0c4dc3f2d98c3fe5e029055e12c92
James <mirageapache@gmail.com>
2022-12-30 13:37:11 +0800

    Add generate_password function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // inclued packages and define server related variables
 const express = require('express')
 const exphbs = require('express-handlebars')
+const generatePassword =  require('./generate_password')
 const app = express()
 const port = 3000
 
@@ -17,8 +18,8 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-  console.log('req.body', req.body)
-  res.render('index')
+  const password = generatePassword(req.body)
+  res.render('index', {password: password})
 })
 
 
---
+++ generate_password.js
@@ -0,0 +1,47 @@
+function generatePassword(options) {
+  // 宣告
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const UpperCaseLetters = lowerCaseLetters.toUpperCase()
+  const number = '1234567890'
+  const symbols = '`~!@$%^&*()-_+={}[]|;:"<>,.?/'
+  let collection = []
+
+  // include 需要的英、數字及符號
+  if( options.lowercase === 'on'){
+    collection = collection.concat(lowerCaseLetters.split(''))
+  }
+
+  if( options.uppercase === 'on'){
+    collection = collection.concat(UpperCaseLetters.split(''))
+  }
+
+  if( options.numbers === 'on'){
+    collection = collection.concat(number.split(''))
+  }
+  
+  if( options.symbols === 'on'){
+    collection = collection.concat(symbols.split(''))
+  }
+  
+  // 去除不要產生的字元
+  if(options.excludeCharacters){
+    collection = collection.filter((character) => !options.excludeCharacters.includes(character))
+  }
+
+  let password = ''
+  for(let i = 1; i<= options.length; i++){
+    password += sample(collection)
+  }
+  console.log(password)
+  return password
+
+}
+
+// 隨機產生字元
+function sample(collection){
+  let randomIndex = Math.floor(Math.random() * collection.length)
+  return collection[randomIndex]
+}
+
+// 匯出function
+module.exports = generatePassword
